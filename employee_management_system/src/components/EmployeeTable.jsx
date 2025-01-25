43a7fad13a7ebc6e183bcbcb1730655272a2c9b8
Jyoti152317 <engjyoti18@gmail.com>
2025-01-25 16:46:19 +0530

    All the needed files are added

---
+++ employee_management_system/src/components/EmployeeTable.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import AddEmployee from "./AddEmployeeForm";
+
+const Employee = () => {
+    const [employee, setEmployee] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [showForm, setShow]=useState(false)
+
+    
+
+    useEffect(() => {
+        fetch()
+    },[])
+
+    const fetch = () => {
+        axios.get(`http://localhost:3000/employees`)
+            .then(res => {
+                console.log(res.data);
+                setEmployee(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+                setError(err)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
+    const deleteBtn = (id) => {
+        
+    }
+
+    if (isLoading) {
+        return<p>Loding...</p>
+    }
+    if (error) {
+        return <p>data is not found</p>
+    }
+
+    return (
+            <div>
+            <h2>Employee Management Dashboard</h2>
+            
+            <div><button style={{ background: "royalblue" }} onClick={showForm}>Add Employee</button>
+            <AddEmployee/></div>
+            <table>
+                <thaed>
+                <tr>
+                                <th>Name</th>
+                                <th>Designamtion</th>
+                                <th>Department</th>
+                                <th>Action</th>
+                            </tr>
+                </thaed>
+                <tbody>
+                {
+                        employee.map((ele) => {
+                            <tr key={ele.id}>
+                                <td>{ele.name}</td>
+                                <td>{ele.designation}</td>
+                                <td>{ele.department}</td>
+                                <td ><button style={{background:"Red"}} onClick={deleteBtn(ele.id)}>Delete</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+                   
+                </table>
+            </div>
+    )
+}
+
+export default Employee;
